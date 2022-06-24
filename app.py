d60edfab226141c4f964ac09532d732f5d71e3c3
germanmark <55302166+germanmark@users.noreply.github.com>
2022-06-23 20:57:31 -0600

    Splitting endpoints into functions

--- app.py
+++ app.py
@@ -3,45 +3,46 @@ import json
 
 app = Flask(__name__)
 
+my_animals = ['Lion', 'Zebra', 'Panther', 'Donkey']
 
-@app.route("/animals",methods=['GET', 'POST', 'PATCH', 'DELETE'])
-def animals():
-    my_animals = ['Lion', 'Zebra', 'Panther', 'Donkey']
-    if request.method == 'GET':
-        return Response(json.dumps(my_animals),
-                        mimetype='application/json',
-                        status=200)
-    elif request.method == 'POST':
-        new_animal = request.json.get('animal')
-        if (new_animal != None):
-            my_animals.append(new_animal)
-            print(my_animals)
-            return Response("Animal successfully added",
-                            mimetype="text/plain",
-                            status=201)
-        else:
-            return Response("Error, no animal specified",
-                            mimetype="text/plain",
-                            status=400)
+@app.get("/animals")
+def animals_get():
+    return Response(json.dumps(my_animals),
+                    mimetype='application/json',
+                    status=200)
 
-    elif request.method == 'PATCH':
-        new_animal = request.json.get('animal')
-        if (new_animal != None):
-            my_animals[3] = new_animal
-            print(my_animals)
-            return Response("Animal successfully changed",
-                            mimetype="text/plain",
-                            status=200)
-        else:
-            return Response("Error, no animal specified",
-                            mimetype="text/plain",
-                            status=400)
-
-    elif request.method == 'DELETE':
-        my_animals.remove('Donkey')
+@app.post("/animals")
+def animals_post():
+    new_animal = request.json.get('animal')
+    if (new_animal != None):
+        my_animals.append(new_animal)
         print(my_animals)
-        return Response("Animal successfully deleted",
+        return Response("Animal successfully added",
+                        mimetype="text/plain",
+                        status=201)
+    else:
+        return Response("Error, no animal specified",
+                        mimetype="text/plain",
+                        status=400)
+@app.patch("/animals")
+def animals_patch():
+    new_animal = request.json.get('animal')
+    if (new_animal != None):
+        my_animals[3] = new_animal
+        print(my_animals)
+        return Response("Animal successfully changed",
                         mimetype="text/plain",
                         status=200)
+    else:
+        return Response("Error, no animal specified",
+                        mimetype="text/plain",
+                        status=400)
+@app.delete("/animals")
+def animals_delete():
+    my_animals.remove('Donkey')
+    print(my_animals)
+    return Response("Animal successfully deleted",
+                    mimetype="text/plain",
+                    status=200)
 
 app.run(debug=True)
